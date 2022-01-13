4bae50cfe1895fe5512d9a9e7740206f2493b56f
Vasileios Michailidis <59122717+VMichailidis@users.noreply.github.com>
2022-01-13 16:36:27 +0200

    Update Superdense.py

--- flask/quantum_algorithms/Superdense.py
+++ flask/quantum_algorithms/Superdense.py
@@ -2,13 +2,15 @@
 #from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute, Aer, IBMQ
 #from qiskit.circuit import Gate
 from .Algorithms import Protocol
+
 class SuperdenseCode:
     # Alice = Sender q[1]
     # Bob = Reciever q[0]
     def __init__(self):
-        self.q = QuantumRegister(2,"q") # quantum register with 2 qubits
-        self.c = ClassicalRegister(2,"c") # classical register with 2 bits
-        self.qc = QuantumCircuit(self.q, self.c) # quantum circuit with quantum and classical registers
+        self.q = 0
+        self.c = 0
+        self.qc = 0
+        self.create_circuit()
     def run(self, data):
         
         self.initializeBellPair(self.q[0], self.q[1], self.qc)
@@ -19,11 +21,20 @@ class SuperdenseCode:
         
         self.decodeQbits(self.q[0], self.q[1], self.qc)
 
-        self.drawCircuit(self.qc)
+#        self.drawCircuit(self.qc)
+#        
+#        self.measureQbits(self.q, self.c, self.qc)
+#        
+#        self.measureResults(self.qc, data)
         
-        self.measureQbits(self.q, self.c, self.qc)
+    def create_circuit(self):
+        self.q = QuantumRegister(2,"q") # quantum register with 2 qubits
+        self.c = ClassicalRegister(2,"c") # classical register with 2 bits
+        self.qc = QuantumCircuit(self.q, self.c) # quantum circuit with quantum and classical registers
         
-        self.measureResults(self.qc, data)
+        data = "00"
+        self.run(data)
+        
     def initializeBellPair(self, q0, q1, qc):
         # apply h-gate to the Alice's qubit
         qc.h(q1)
@@ -50,6 +61,8 @@ class SuperdenseCode:
     
         # apply h-gate to the Alice's qubit
         qc.h(q1)
+  
+        
     def drawCircuit(self, qc):
         # draw the circuit in Qiskit's reading order
         display(qc.draw(output='mpl',reverse_bits=True))
@@ -63,6 +76,3 @@ class SuperdenseCode:
         job = execute(qc,Aer.get_backend('qasm_simulator'),shots=100)
         counts = job.result().get_counts(qc)
         print(data,"--->",counts)
-    def save(self):
-        pass
-
