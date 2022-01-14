53755ad31ad1ef93af6209dc9c3128329fb45bf5
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2022-01-14 23:44:09 +0200

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,19 +1,21 @@
 from flask import Flask, redirect, render_template, request, session
-from quantum_algorithms.my_qiskit import circuit
-# from quantum_algorithms.Cryptography import Cryptography_Protocol as Cryptography
-# from quantum_algorithms.Superdense import Superdense_Protocol as Superdense
-# from quantum_algorithms.Teleportation import Teleportation_Protocol as Teleportation
+from quantum_algorithms.my_qiskit import choose_algorithm
+# from quantum_algorithms.Cryptography import Cryptography_Protocol
+# from quantum_algorithms.Superdense import Superdense_Protocol
+# from quantum_algorithms.Teleportation import Teleportation_Protocol
 
-images_folder = "../react/public/static/"
+IMAGES_FOLDER = "../react/public/static/"
 
 # Initialize images
-circuit( 'cryptography', images_folder, 3 )
-circuit( 'teleportation', images_folder, 3 )
-circuit( 'superdense', images_folder, 3 )
+print("------------------------------------------------------- MYRON")
+choose_algorithm( 'cryptography', IMAGES_FOLDER, 3 )
+print("------------------------------------------------------- VASILIS")
+choose_algorithm( 'superdense', IMAGES_FOLDER, 3 )
+print("------------------------------------------------------- IOANNA")
+choose_algorithm( 'teleportation', IMAGES_FOLDER, 3 )
 
 
 ##------------------------------------------------- FLASK APP
-
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
@@ -21,11 +23,10 @@ def index():
     # for i in request.args:
     #     print(i, request.args.get(i))
     
-    circuit( request.args.get('algorithm'), images_folder, int(request.args.get('q_num')) )
+    choose_algorithm( request.args.get('algorithm'), IMAGES_FOLDER, int(request.args.get('q_num')) )
     return redirect("")
 
 
 if __name__ == '__main__':
     app.run(host='localhost', port=80)
     #ON CMD: flask run --host=localhost --port=80
-
