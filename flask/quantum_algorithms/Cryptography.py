efb690e3201cf3a598f748ea696a4c6ebd31e24a
Myron Giannakis <86615442+G-Myron@users.noreply.github.com>
2022-01-14 23:24:29 +0200

    Updated Cryptography and Superdense

--- flask/quantum_algorithms/Cryptography.py
+++ flask/quantum_algorithms/Cryptography.py
@@ -2,19 +2,17 @@ from qiskit.circuit import Gate
 from .Algorithm import Protocol
 
 class Cryptography_Protocol(Protocol):
-    # def __init__(self, save_folder, q_num):
-    #     super().__init__(save_folder, 1)
     
     def create_circuit(self):
-        self.qc.h(self.qc.qubits)
-        self.qc.cx(0, self.q_num-1)
-
         # self.qc.append(Gate("Alice",1,[]), self.qc.qregs)
 
-        # self.qc.h(0) # if i==0 else qc.i(0)
-        # self.qc.barrier() # Loooooong distance
-        # self.qc.barrier() # Loooooong distance
-        # self.qc.h(0) # if j==0 else qc.i(0)
+        for i in range(2):
+            for j in range(2):
+                self.qc.h(0) if i==0 else self.qc.i(0)
+                self.qc.barrier() # Loooooong distance
+                self.qc.barrier() # Loooooong distance
+                self.qc.h(0) if j==0 else self.qc.i(0)
+                self.qc.barrier()
         
         # self.qc.append(Gate("Bob",1,[]), self.qc.qregs)
 
--- flask/quantum_algorithms/Superdense.py
+++ flask/quantum_algorithms/Superdense.py
@@ -1,79 +1,37 @@
-#import numpy as np
-#from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute, Aer, IBMQ
-#from qiskit.circuit import Gate
-from .Algorithms import Protocol
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from .Algorithm import Protocol
 
-class SuperdenseCode:
-    # Alice = Sender q[1]
-    # Bob = Reciever q[0]
-    def __init__(self):
-        super().__init__()
-        self.q = 0
-        self.c = 0
-        self.qc = 0
-        self.create_circuit()
-    def run(self, data):
-        
-        self.initializeBellPair(self.q[0], self.q[1], self.qc)
- 
-        self.encodeQbits(data, self.q[1], self.qc)
-        
-        self.sendQbit(self.qc)
-        
-        self.decodeQbits(self.q[0], self.q[1], self.qc)
-
-#        self.drawCircuit(self.qc)
-#        
-#        self.measureQbits(self.q, self.c, self.qc)
-#        
-#        self.measureResults(self.qc, data)
-        
+class Superdense_Protocol(Protocol):
+    """Alice = Sender q[1] \n Bob = Reciever q[0]"""
+    
     def create_circuit(self):
         self.q = QuantumRegister(2,"q") # quantum register with 2 qubits
         self.c = ClassicalRegister(2,"c") # classical register with 2 bits
         self.qc = QuantumCircuit(self.q, self.c) # quantum circuit with quantum and classical registers
-        
-        data = "00"
-        self.run(data)
-        
-    def initializeBellPair(self, q0, q1, qc):
-        # apply h-gate to the Alice's qubit
-        qc.h(q1)
 
-        # apply cx-gate to Alice's qubit, controlled by Bob's qubit
-        qc.cx(q1, q0)
-        
-    def encodeQbits(self, data, q, qc):
+        self.initializeBellPair(self.q[0], self.q[1])
+        self.encodeQbits(self.q[1], "00")
+        self.sendQbit()
+        self.decodeQbits(self.q[0], self.q[1])
+    
+    def initializeBellPair(self, q0, q1):
+        self.qc.h(q1) # apply h-gate to the Alice's qubit
+        self.qc.cx(q1, q0) # apply cx-gate to Alice's qubit, controlled by Bob's qubit
+    
+    ### PROBLEM WITH q: slef.q or self.q[1]?
+    def encodeQbits(self, q, data="00"):
         # if a is 1, then apply z-gate to Alice's qubit
         if data[0]=='1':
-            qc.z(q)
-    
+            self.qc.z(q)
+
         # if b is 1, then apply x-gate to Alice's qubit
         if data[1]=='1':
             self.qc.x(self.q)
     
-    def sendQbit(self, qc):
-        # Alice sends her qubit to Bob
-        qc.barrier()
+    def sendQbit(self):
+        self.qc.barrier() # Alice sends her qubit to Bob
     
-    def decodeQbits(self, q0, q1, qc):
-        #  apply cx-gate to Alice's qubit, controlled by Bob's qubit
-        qc.cx(q1, q0)
-    
-        # apply h-gate to the Alice's qubit
-        qc.h(q1)
-  
-        
-    def drawCircuit(self, qc):
-        # draw the circuit in Qiskit's reading order
-        display(qc.draw(output='mpl',reverse_bits=True))
-        
-    def measureQbits(self, q, c, qc):
-        # measure both qubits
-        self.qc.measure(self.q,self.c)
-       
-    def measureResults(self, qc, data):
-        # compare the results with pair (a,b)
-        job = execute(qc,Aer.get_backend('qasm_simulator'),shots=100)
-        counts = job.result().get_counts(qc)
-        print(data,"--->",counts)
+    def decodeQbits(self, q0, q1):
+        self.qc.cx(q1, q0) #  apply cx-gate to Alice's qubit, controlled by Bob's qubit
+        self.qc.h(q1) # apply h-gate to the Alice's qubit
+
